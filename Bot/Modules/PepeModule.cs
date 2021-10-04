244bda67b8851a8a220d8c17262e3f2963a2114f
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-10-04 23:27:00 +0100

    Create PepeModule.cs

---
+++ Bot/Modules/PepeModule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Common;
+using Data.Models;
+using Discord.Commands;
+
+namespace Bot.Modules
+{
+    public class PepeModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PepeModule(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Command("pepe")]
+        public async Task GetPepe()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetStringAsync("https://meme-api.herokuapp.com/gimme/pepethefrog");
+            var pepe = Meme.FromJson(response);
+
+            if (pepe == null)
+            {
+                await ReplyAsync(
+                    "Pepe is too hungover to return any pictures of last night.. Fuck off and try again later. While your at it let Cillian know that he's an idiot and needs to fix Pepe.");
+                return;
+            }
+
+            var pepeEmbed = new SP1XEmbedBuilder()
+                .WithTitle($"{pepe.Title}")
+                .WithImage($"{pepe.Url}")
+                .WithStyle(EmbedStyle.Image)
+                .Build();
+
+            await Context.Channel.SendMessageAsync(embed: pepeEmbed);
+            return;
+        }
+    }
+}
