9bc496179521087c2806dd1118f6c355ca1ed476
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-10-04 22:49:47 +0100

    Memes
    This commit adds a new meme command, which allows users to query an API endpoint and return a meme from a subreddit.
    
    This commit also adds a Serializer and Convertor for the API endpoint making it easier to parse.
    
    We also added a HTTPClientFactory to facilitate querying these endpoints in a compliant way.

---
+++ Data/Models/Meme.cs
@@ -0,0 +1,47 @@
+﻿namespace Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Meme
+    {
+        [JsonProperty("postLink")]
+        public Uri PostLink { get; set; }
+
+        [JsonProperty("subreddit")]
+        public string Subreddit { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("nsfw")]
+        public bool Nsfw { get; set; }
+
+        [JsonProperty("spoiler")]
+        public bool Spoiler { get; set; }
+
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("ups")]
+        public long Ups { get; set; }
+
+        [JsonProperty("preview")]
+        public Uri[] Preview { get; set; }
+    }
+
+    public partial class Meme
+    {
+        public static Meme FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<Meme>(json, Converter.Settings);
+        }
+    }
+}
