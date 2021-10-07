6d23c957cc6037f8f2526d1e2aa065fd3d290a51
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-10-07 22:43:00 +0100

    MYS Modules

---
+++ Bot/Modules/Subjects/MYS/MYSDelete.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Common;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace Bot.Modules.Subjects.MYS
+{
+    public class MYSDelete : SP1XModuleBase
+    {
+        public MYSDelete(IServiceProvider serviceProvider, IConfiguration configuration) : base(
+            serviceProvider,
+            configuration)
+        {
+        }
+
+        [Command("mysdelete", RunMode = RunMode.Async)]
+
+        public async Task MYSDeleteCmd(string name, [Remainder] string argument)
+        {
+            var subject = "MYS";
+
+            var socketGuildUser = Context.User as SocketGuildUser;
+
+            var mysAssignment = await _DataAccessLayer.GetAssignment(subject, name);
+            if (mysAssignment == null)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Not Found")
+                    .WithDescription("The assignment you requested could not be found.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+
+            if (mysAssignment.OwnerId != Context.User.Id && !socketGuildUser.GuildPermissions.Administrator)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Access Denied!")
+                    .WithDescription("You need to be a student or administrator to create an assignment.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+
+            await _DataAccessLayer.DeleteAssignmentWithSubjectName(subject, name);
+
+            var deleted = new SP1XEmbedBuilder()
+                .WithTitle("Assignment Deleted!")
+                .WithDescription("The assignment was successfully deleted.")
+                .WithStyle(EmbedStyle.Success)
+                .Build();
+
+            await Context.Channel.SendMessageAsync(embed: deleted);
+        }
+    }
+}
---
+++ Bot/Modules/Subjects/MYS/MYSEdit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Common;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace Bot.Modules.Subjects.MYS
+{
+    public class MYSEdit : SP1XModuleBase
+    {
+        public MYSEdit(IServiceProvider serviceProvider, IConfiguration configuration) : base(serviceProvider, configuration)
+        {
+        }
+
+        [Command("mysedit", RunMode = RunMode.Async)]
+        public async Task MYSEditCmd(string name, string newname, [Remainder] string argument)
+        {
+            var arguments = argument.Split(" ");
+            var subject = "MYS";
+
+            var socketGuildUser = Context.User as SocketGuildUser;
+
+            var ppdAssignment = await _DataAccessLayer.GetAssignment(subject, name);
+            if (ppdAssignment == null)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Not Found")
+                    .WithDescription("The assignment you requested could not be found.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+            if (ppdAssignment.OwnerId != Context.User.Id && !socketGuildUser.GuildPermissions.Administrator)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Access Denied!")
+                    .WithDescription("You need to be a student or administrator to create an assignment.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+
+            await _DataAccessLayer.EditAssignmentContentSubject(subject, name, newname, argument);
+
+            var edited = new SP1XEmbedBuilder()
+                .WithTitle($"You have successfully edited the assignment {name}")
+                .WithDescription($"Assignment {name} now renamed to {newname}. \n \n Assignment Content changed to: \n {argument}")
+                .WithStyle(EmbedStyle.Error)
+                .Build();
+            await Context.Channel.SendMessageAsync(embed: edited);
+        }
+    }
+}
