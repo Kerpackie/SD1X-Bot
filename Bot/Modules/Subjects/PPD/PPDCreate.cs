0d1d5dd95f9d46191f65c9516e4e68810feb5a07
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-10-07 22:43:15 +0100

    PPD Modules

---
+++ Bot/Modules/Subjects/PPD/PPDCreate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Common;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace Bot.Modules.Subjects.PPD
+{
+    public class PPDCreate : SP1XModuleBase
+    {
+        public PPDCreate(IServiceProvider serviceProvider, IConfiguration configuration) : base(serviceProvider,
+            configuration)
+        {
+        }
+
+        [Command("ppdcreate", RunMode = RunMode.Async)]
+
+        public async Task PPDCreateCmd(string name, [Remainder] string argument)
+        {
+            var arguments = argument.Split(" ");
+            var subject = "PPD";
+
+            var socketGuildUser = Context.User as SocketGuildUser;
+
+            var ppdAssignment = await _DataAccessLayer.GetAssignment(subject, name);
+            if (ppdAssignment == null)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Not Found")
+                    .WithDescription("The assignment you requested could not be found.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+
+            if (ppdAssignment.OwnerId != Context.User.Id && !socketGuildUser.GuildPermissions.Administrator)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Access Denied!")
+                    .WithDescription("You need to be a student or administrator to create an assignment.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+
+            await _DataAccessLayer.CreateAssignment(subject, Context.User.Id, arguments[0],
+                string.Join(" ", arguments.Skip(1)));
+
+            var prefix = "!";
+
+            var created = new SP1XEmbedBuilder()
+                .WithTitle("Assignment Created!")
+                .WithDescription(
+                    $"The Assignment has been successfully created. You can view it by using `{prefix}ppd {arguments[1]}`")
+                .WithStyle(EmbedStyle.Success)
+                .Build();
+            await Context.Channel.SendMessageAsync(embed: created);
+        }
+    }
+}
