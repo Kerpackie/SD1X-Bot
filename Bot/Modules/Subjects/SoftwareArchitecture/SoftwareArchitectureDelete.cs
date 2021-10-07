99b0420a8075921c016860c5a3b3398cb8c2ac97
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-10-07 22:43:28 +0100

    Software Architecture Modules

---
+++ Bot/Modules/Subjects/SoftwareArchitecture/SoftwareArchitectureDelete.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Common;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace Bot.Modules.Subjects.SoftwareArchitecture
+{
+    public class SoftwareArchitectureDelete : SP1XModuleBase
+    {
+        public SoftwareArchitectureDelete(IServiceProvider serviceProvider, IConfiguration configuration) : base(
+            serviceProvider,
+            configuration)
+        {
+        }
+
+        [Command("softwarearchdelete", RunMode = RunMode.Async)]
+        [Alias("softwaredelete")]
+
+        public async Task SoftwareArchitectureDeleteCmd(string name, [Remainder] string argument)
+        {
+            var subject = "Software Architecture";
+
+            var socketGuildUser = Context.User as SocketGuildUser;
+
+            var softwareAssignment = await _DataAccessLayer.GetAssignment(subject, name);
+            if (softwareAssignment == null)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Not Found")
+                    .WithDescription("The assignment you requested could not be found.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+
+            if (softwareAssignment.OwnerId != Context.User.Id && !socketGuildUser.GuildPermissions.Administrator)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Access Denied!")
+                    .WithDescription("You need to be a student or administrator to create an assignment.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+
+            await _DataAccessLayer.DeleteAssignmentWithSubjectName(subject, name);
+
+            var deleted = new SP1XEmbedBuilder()
+                .WithTitle("Assignment Deleted!")
+                .WithDescription("The assignment was successfully deleted.")
+                .WithStyle(EmbedStyle.Success)
+                .Build();
+
+            await Context.Channel.SendMessageAsync(embed: deleted);
+        }
+    }
+}
---
+++ Bot/Modules/Subjects/SoftwareArchitecture/SoftwareArchitectureEdit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Common;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace Bot.Modules.Subjects.SoftwareArchitecture
+{
+    public class SoftwareArchitectureEdit : SP1XModuleBase
+    {
+        public SoftwareArchitectureEdit(IServiceProvider serviceProvider, IConfiguration configuration) : base(serviceProvider, configuration)
+        {
+        }
+
+        [Command("softwarearchedit", RunMode = RunMode.Async)]
+        [Alias("softwareedit")]
+        public async Task SoftwareArchitectureEditCmd(string name, string newname, [Remainder] string argument)
+        {
+            var arguments = argument.Split(" ");
+            var subject = "Software Architecture";
+
+            var socketGuildUser = Context.User as SocketGuildUser;
+
+            var softwareAssignment = await _DataAccessLayer.GetAssignment(subject, name);
+            if (softwareAssignment == null)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Not Found")
+                    .WithDescription("The assignment you requested could not be found.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+            if (softwareAssignment.OwnerId != Context.User.Id && !socketGuildUser.GuildPermissions.Administrator)
+            {
+                var embed = new SP1XEmbedBuilder()
+                    .WithTitle("Access Denied!")
+                    .WithDescription("You need to be a student or administrator to create an assignment.")
+                    .WithStyle(EmbedStyle.Error)
+                    .Build();
+
+                await Context.Channel.SendMessageAsync(embed: embed);
+                return;
+            }
+
+            await _DataAccessLayer.EditAssignmentContentSubject(subject, name, newname, argument);
+
+            var edited = new SP1XEmbedBuilder()
+                .WithTitle($"You have successfully edited the assignment {name}")
+                .WithDescription($"Assignment {name} now renamed to {newname}. \n \n Assignment Content changed to: \n {argument}")
+                .WithStyle(EmbedStyle.Error)
+                .Build();
+            await Context.Channel.SendMessageAsync(embed: edited);
+        }
+    }
+}
