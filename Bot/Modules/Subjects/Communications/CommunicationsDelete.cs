feeb872636a1aa25f333197a5e9b1a661dda8bc4
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-10-11 19:32:13 +0100

    Update missing using statements

--- Bot/Modules/Subjects/Communications/CommunicationsDelete.cs
+++ Bot/Modules/Subjects/Communications/CommunicationsDelete.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bot.Common;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
